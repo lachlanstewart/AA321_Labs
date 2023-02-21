61f6f2e83a983df74f24ad1539f9d1c946bfd513
Minatani <minatani@uw.edu>
2023-02-20 19:17:51 -0800

    Added comments for lachlan

--- AA321_Lab5.py
+++ AA321_Lab5.py
@@ -2,18 +2,22 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def weightBal(dataMatrix,balanceMatrix,tareMatrix):
+def tareData(dataMatrix,balanceMatrix,tareMatrix):
     
     k = len(dataMatrix)
     
+    # LDP is matrix with Columns of: AlphaENC, Lift, Drag, PitchMoment
     LDP = np.zeros((k,4))
-    LDP[:,0] = dataMatrix[:,2]
+    LDP[:,0] = dataMatrix[:,2] #AlphaENC Column filled
     
+    # Columns from tare reference table to be used by np.interp()
     tareCLW = tareMatrix[:,0]
     tareL = tareMatrix[:,1]
     tareD = tareMatrix[:,2]
     tarePM = tareMatrix[:,3]   
     
+    # For every row of data, adjust L,D,PM for weight and fill to LDP, then subtract strut interference
+    # Note that Strut Interference is semi-normalized, dataMatrix[x,5] is Q_Actual
     for x in range(k):
         tempArray = np.zeros((3,1))
         CLW = dataMatrix[x,12] / (9.375 * dataMatrix[x,5])
@@ -32,37 +36,32 @@ def weightBal(dataMatrix,balanceMatrix,tareMatrix):
         
 def process_data():
     # Retrieve Data
-    Run35 = pd.read_csv(r'Raw Data\RUN_0035.csv', header=1)
-    Run36 = pd.read_csv(r'Raw Data\RUN_0036.csv', header=1)
+    Run35 = pd.read_csv(r'Raw Data\RUN_0035.csv')
+    Run36 = pd.read_csv(r'Raw Data\RUN_0036.csv')
     Run37 = pd.read_csv(r'Raw Data\RUN_0037.csv')
-    Run38 = pd.read_csv(r'Raw Data\RUN_0038.csv', header=1)
-    Run39 = pd.read_csv(r'Raw Data\RUN_0039.csv', header=1)
-    Run40 = pd.read_csv(r'Raw Data\RUN_0040.csv', header=1)
+    Run38 = pd.read_csv(r'Raw Data\RUN_0038.csv')
+    Run39 = pd.read_csv(r'Raw Data\RUN_0039.csv')
+    Run40 = pd.read_csv(r'Raw Data\RUN_0040.csv')
     
-    tare10q = (pd.read_csv(r'Data Correction Resources\10Q_CLW.csv')).to_numpy()
+    tare10q = (pd.read_csv(r'Data Correction Resources\10Q_CLW.csv')).to_numpy() # I figured out I can to_numpy in same line. Too lazy to retroactively fix others
     tare35q = (pd.read_csv(r'Data Correction Resources\35Q_CLW.csv')).to_numpy()
     
-    preMatrix = pd.read_csv(r'Data Correction Resources\Balance Interaction Matrix.csv', skip_blank_lines=True, index_col=0, header=[1,2])
-    balanceMatrix = preMatrix.to_numpy()
+    preBalanceMatrix = pd.read_csv(r'Data Correction Resources\Balance Interaction Matrix.csv', skip_blank_lines=True, index_col=0, header=[1,2])
     
-    
+    balanceMatrix = preBalanceMatrix.to_numpy()
     run37M = Run37.to_numpy()
     run38M = Run38.to_numpy()
     run39M = Run39.to_numpy()
     run40M = Run40.to_numpy()
     
-    LDP37 = weightBal(run37M,balanceMatrix,tare10q)
-    LDP38 = weightBal(run38M,balanceMatrix,tare10q)
-    LDP39 = weightBal(run39M,balanceMatrix,tare35q)
-    LDP40 = weightBal(run40M,balanceMatrix,tare35q)
-    print(LDP38) # for debugging
-
-    
-    # print('Balance Matrix: \n', balanceMatrix) # for debugging
-    # print('\n Run 37: \n', run37M)             # for debugging
+    # Execute tare function
+    LDP37 = tareData(run37M,balanceMatrix,tare10q)
+    LDP38 = tareData(run38M,balanceMatrix,tare10q)
+    LDP39 = tareData(run39M,balanceMatrix,tare35q)
+    LDP40 = tareData(run40M,balanceMatrix,tare35q)
     
 def main():
-    # Read data
+    #Yeah I kinda did all my code in process_data(). Probably not good practice but here we are m8
     process_data()
     print('Main Successfully Executed')
     
